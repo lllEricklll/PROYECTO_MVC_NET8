1add30b352e8ce6a39b9198eb978c519cbc4b4da
lllEricklll <erickpadillajl@gmail.com>
2024-01-12 20:38:28 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET8_Proyecto_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
